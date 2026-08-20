e8c997fcfb7af92c0f181212f9ec7f11eadeb46e
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2026-08-20 01:24:55 -0300

    feat: Pesquisa de cotação de imposto

---
+++ src/infra/exchange_rate/exchange_rate_class.py
@@ -0,0 +1,53 @@
+from infra.object_value.date_object import DateObjectBuild
+
+import json
+from urllib.request import urlopen, Request
+
+class ExchangeRateData:
+    def __init__(self):
+        self.date = ''
+        self.exchangeRate = ''
+
+class ExchangeRateDataBuild:
+    def __init__(self):
+        self._exchangeRateData = ExchangeRateData()
+
+    def setDate(self, date):
+        self._exchangeRateData.date = (DateObjectBuild()
+            .setDate(date)
+            .setElementsDate()
+            .build()
+            )
+        
+        return self
+
+    def setExchangeRate(self):
+        dateFormatExchangeRate = self._exchangeRateData.date.year + '-' + self._exchangeRateData.date.month + '-' + self._exchangeRateData.date.day
+
+        exchangeRateURL = "https://api.frankfurter.dev/v2/rates?base=JPY&quotes=BRL&date=" + dateFormatExchangeRate
+
+        request = Request(exchangeRateURL, headers={"Accept": "application/json"})
+
+        with urlopen(request) as response:
+            raw_data = response.read().decode("utf-8")
+            
+            data = json.loads(raw_data)
+
+        self._exchangeRateData.exchangeRate = data[0]['rate']
+        return self
+
+    def build(self):
+        self.validate()
+        return self._exchangeRateData
+
+    def validateEmptyValues(self, nameProperties):
+        for nameProperty in nameProperties:
+            propertyValue = getattr(
+                self._exchangeRateData,
+                nameProperty
+            )
+
+            if not propertyValue:
+                raise ValueError(
+                    nameProperty + " cannot be an empty value"
+                )
