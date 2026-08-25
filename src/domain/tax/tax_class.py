a90e7698507d22176b6e8290c57e21ba4653957d
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2026-08-25 18:43:05 -0300

    feat: Funcionalidade de taxa

--- src/domain/tax/tax_class.py
+++ src/domain/tax/tax_class.py
@@ -1,50 +1,45 @@
-from enum import Enum
-
-class Status(Enum):
-    NO_DATA = 'Sem informações'
-    AWAITING_PAYMENT = 'Esperando pagamento'
-    COMPLETED = 'Taxa de imposto foi paga'
+from .tax_enum import TaxStatus
 
 class ProductTax:
     def __init__(self):
-        self.dateLimit = ''
-        self.taxDate = ''
-        self.taxPrice = ''
-        self.status = Status.NO_DATA
+        self.taxDateLimit = None
+        self.taxDate      = None
+        self.taxPrice     = "0"
+        self.taxStatus    = TaxStatus.NO_DATA
 
 class ProductTaxBuild:
     def __init__(self):
         self._productTax = ProductTax()
 
-    def setDateLimit(self, dateLimit):        
-        self._productTax.dateLimit = dateLimit
-        self.updateStatus(Status.AWAITING_PAYMENT)
+    def setDateLimit (self, taxDateLimit):        
+        self._productTax.taxDateLimit = taxDateLimit
+        self.updateTaxStatus(TaxStatus.AWAITING_PAYMENT)
         return self
     
-    def setTaxDate(self, taxDate):        
+    def setTaxDate (self, taxDate):        
         self._productTax.taxDate = taxDate
-        self.updateStatus(Status.COMPLETED)
+        self.updateTaxStatus(TaxStatus.COMPLETED)
         return self
 
-    def setTaxPrice(self, taxPrice):        
+    def setTaxPrice (self, taxPrice):        
         self._productTax.taxPrice = taxPrice
         return self
 
-    def updateStatus(self, status):
-        self._productTax.status = status
+    def updateTaxStatus (self, taxStatus):
+        self._productTax.taxStatus = taxStatus
         return self
 
-    def build(self):
+    def build (self):
         self.validate()
         return self._productTax.__dict__
 
-    def validate(self):
-        if self._productTax.status == Status.NO_DATA:
+    def validate (self):
+        if self._productTax.taxStatus == TaxStatus.NO_DATA:
             return
-        elif self._productTax.status == Status.AWAITING_PAYMENT:
-            self.validateEmptyValues(["dateLimit"])
-        elif self._productTax.status == Status.COMPLETED:
-            self.validateEmptyValues(["dateLimit", "taxDate", "taxPrice"])
+        elif self._productTax.taxStatus == TaxStatus.AWAITING_PAYMENT:
+            self.validateEmptyValues(["taxDateLimit"])
+        elif self._productTax.taxStatus == TaxStatus.COMPLETED:
+            self.validateEmptyValues(["taxDateLimit", "taxDate", "taxPrice"])
         else:
             raise ValueError("Invalid status error")
 
--- src/domain/tax/tax_factory.py
+++ src/domain/tax/tax_factory.py
@@ -5,20 +5,22 @@ class ProductTaxFactory:
 
         has_tax = product_info.get("Imposto", None)
         if not has_tax:
-            return (ProductTaxBuild()
+            return (
+                    ProductTaxBuild()
                     .build()
-                    )
+                )
 
         has_date_tax = product_info["Imposto"].get("Data_do_Imposto", None)
         if not has_date_tax:
-            return (ProductTaxBuild()
-            .setDateLimit(product_info["Frete"]["Data_Limite_Pagamento"])
-            .build()
-            )
-
-        return (ProductTaxBuild()
-                .setDateLimit(product_info["Frete"]["Data_Limite_Pagamento"])
-                .setTaxDate(product_info["Frete"]["Data_do_Imposto"])
-                .setTaxPrice(product_info["Frete"]["Preco_Imposto"])
+            return (
+                ProductTaxBuild()
+                .setDateLimit(product_info["Imposto"]["Data_Limite_Pagamento"])
                 .build()
-                )
+            )
+        return (
+                ProductTaxBuild()
+                .setDateLimit(product_info["Imposto"]["Data_Limite_Pagamento"])
+                .setTaxDate(product_info["Imposto"]["Data_do_Imposto"])
+                .setTaxPrice(product_info["Imposto"]["Preco_Imposto"])
+                .build()
+            )
